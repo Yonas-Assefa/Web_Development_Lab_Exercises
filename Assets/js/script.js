e5cd39161cf9d33b4d4af4c184e95a22085d9c84
Yonas Assefa <se.yonas.assefa@gmail.com>
2021-01-24 20:24:32 -0800

    at this stage,the bank system's deposite,transfer and balance checker functions were made

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,3 +14,42 @@ function deposite(amount){
        
 
 }
+
+
+//function to withdraw
+function withdraw(amount){
+    if(personal_account.deposite >=amount){
+        if(amount >=5 && amount <=5000){
+            personal_account.deposite = personal_account.deposite - Number.parseFloat(amount);
+            console.log("your operation has been completed successfully. Your balance is: " + personal_account.deposite + "birr");
+        }
+        else console.log("sorry! the you can only withdraw between 50birr and 5000birr  Try again");
+    } else console.log("sorry! your balance is insufficient. try again")
+    
+
+}
+
+
+//function to display balance
+function balance(){
+    console.log("Your account balance is: " + personal_account.deposite + "birr");
+}
+
+
+//function to transfer
+function transfer(amount, reciver_ID){
+    console.log(personal_account.deposite);
+    if(personal_account.deposite >= amount){
+        let intermidate = personal_account.deposite - amount;
+        console.log(amount + " birr has benn transferd to a  bank account " + reciver_ID + " successfully. your remaining balance is: " + intermidate);
+    } else console.log("sorry you dont have enough balance. try again.");
+}
+
+
+//function to generate a random account number
+function account_number() { 
+    var minm = 10000; 
+    var maxm = 99999; 
+    return Account_num = Math.floor(Math 
+    .random() * (maxm - minm + 1)) + minm; 
+}
